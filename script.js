1403a1b87d9444149bd4d63e8b45f80e9e8512bf
Shawn <shawnyu967@gmail.com>
2023-10-19 15:49:25 -0400

    decoupling into game board, game logic, and display

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-const gameFlow = (function (doc) {
-  const innerBoard = new Array(9);
-  const cells = [...doc.getElementsByClassName("cell")];
-  const resultDisplay = document.querySelector(".result");
-  const resetBtn = document.querySelector(".reset");
-  cells.forEach((cell) => cell.addEventListener("click", () => cellClicked(cell.getAttribute("data-cell"), cell)));
-  resetBtn.addEventListener("click", resetGame);
-  /*temp*/
+const player = (name, symbol) => {
+  return { name, symbol };
+};
+
+const gameBoard = (() => {
+  const board = new Array(9);
+
+  const putEntry = (index, symbol) => {
+    if (board[index] == undefined) {
+      board[index] = symbol;
+    }
+  };
+
+  const reset = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i] = undefined;
+    }
+  };
+
+  const getIndex = (index) => {
+    return board[index];
+  };
+
+  return { getIndex, putEntry, reset };
+})();
+
+const gameLogic = (() => {
+  const p1 = player("John", "X");
+  const p2 = player("Jane", "O");
 
   let turns = 1;
   let gameOver = false;
+  let curPlayer = p1;
 
-  /*temp*/
-
-  function cellClicked(index, cell) {
-    let symbol = turns % 2 != 0 ? "X" : "O";
-    if (innerBoard[index] == undefined && gameOver == false && turns < 10) {
-      innerBoard[index] = symbol;
-      cell.innerHTML = symbol;
-      //console.log(innerBoard);
-      if (checkWin(symbol)) {
-        console.log("win");
-        resultDisplay.innerHTML = `Player ${symbol} WINS`;
-        gameOver = true;
-      } else {
-        console.log("continue");
-        turns++;
-      }
-    }
-
-    if (turns == 10) {
-      resultDisplay.innerHTML = `TIE`;
-    }
-  }
+  const getCurPlayer = () => {
+    return curPlayer;
+  };
 
   const winCons = [
     [0, 1, 2],
@@ -44,23 +47,74 @@ const gameFlow = (function (doc) {
     [2, 4, 6],
   ];
 
-  function checkWin(symbol) {
+  const validTurn = (index, cell) => {
+    curPlayer = turns % 2 != 0 ? p1 : p2;
+    if (gameBoard.getIndex(index) == undefined && gameOver == false && turns < 10) {
+      gameBoard.putEntry(index, curPlayer.symbol);
+      gameDisplay.updateCell(cell, curPlayer.symbol);
+      if (checkWin(curPlayer.symbol)) {
+        gameOver = true;
+        return 0;
+      } else {
+        turns++;
+        return 1;
+      }
+    }
+    if (turns == 10) {
+      return 2;
+    }
+  };
+
+  const checkWin = (symbol) => {
     for (let i = 0; i < winCons.length; i++) {
       const row = winCons[i];
-      if (row.every((element) => innerBoard[element] == symbol)) {
+      if (row.every((element) => gameBoard.getIndex(element) == symbol)) {
         return true;
       }
     }
     return false;
-  }
+  };
 
-  function resetGame() {
+  const resetGame = () => {
     turns = 1;
     gameOver = false;
-    cells.forEach((cell) => (cell.innerHTML = ""));
-    for (let i = 0; i < innerBoard.length; i++) {
-      innerBoard[i] = undefined;
+    curSymbol = p1.symbol;
+  };
+
+  return { validTurn, getCurPlayer, resetGame };
+})();
+
+const gameDisplay = (function (doc) {
+  const cells = [...doc.getElementsByClassName("cell")];
+  const resultDisplay = document.querySelector(".result");
+  const resetBtn = document.querySelector(".reset");
+  cells.forEach((cell) => cell.addEventListener("click", () => cellClicked(cell.getAttribute("data-cell"), cell)));
+
+  const updateCell = (cell, symbol) => {
+    cell.innerHTML = symbol;
+  };
+
+  const cellClicked = (index, cell) => {
+    const turn = gameLogic.validTurn(index, cell);
+    if (turn == 0) {
+      console.log("win");
+      resultDisplay.innerHTML = `Player ${gameLogic.getCurPlayer().name} wins`;
+    } else if (turn == 1) {
+      console.log("continue");
+    } else if (turn == 2) {
+      resultDisplay.innerHTML = "TIE";
+      console.log("TIE");
     }
+  };
+
+  const resetGame = () => {
+    cells.forEach((cell) => (cell.innerHTML = ""));
+    gameBoard.reset();
+    gameLogic.resetGame();
     resultDisplay.innerHTML = null;
-  }
+  };
+
+  resetBtn.addEventListener("click", resetGame);
+
+  return { updateCell };
 })(document);
